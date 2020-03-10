460e9f8a66e2518cd7f14d43f5d9bad29b9f55da
Lucas Serain <lucas.serain@outlook.com>
2020-03-09 22:24:49 -0300

    SalvarArquivos

--- src/chamaAPI.js
+++ src/chamaAPI.js
@@ -3,13 +3,15 @@ import api from "./apiConfig"
 export default (async function chamaApi(values) {
     api.post('/automatos', values)
       .then(function (response) {
+
+        var FileSaver = require('file-saver');
+        var blob = new Blob([response.data], {type: "text/plain;charset=utf-8"});
+        FileSaver.saveAs(blob, "hello world.cpp");
         console.log(response);
       })
       .catch(function (error) {
         console.log(error);
       });
 
-      var FileSaver = require('file-saver');
-      var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-      FileSaver.saveAs(blob, "hello world.cpp");
+
   });
